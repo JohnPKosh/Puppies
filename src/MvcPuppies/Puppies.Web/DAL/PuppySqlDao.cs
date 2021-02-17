b3d27950873f2bafdcd00b8c86f2364bbe35c061
John Kosh <devprohome@live.com>
2021-02-17 07:52:33 -0500

    added basic PuppySqlDao wip

--- src/MvcPuppies/Puppies.Web/DAL/PuppySqlDao.cs
+++ src/MvcPuppies/Puppies.Web/DAL/PuppySqlDao.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Data.SqlClient;
 using System.Linq;
 using System.Threading.Tasks;
@@ -20,29 +21,125 @@ namespace Puppies.Web.DAL
     /// <summary>
     /// Returns a list of all puppies
     /// </summary>
-    /// <returns></returns>
+    /// <returns><![CDATA[IList<Puppy>]]></returns>
     public IList<Puppy> GetPuppies()
     {
-      throw new NotImplementedException();
+      var rv = new List<Puppy>();
+      try
+      {
+        var connection = new SqlConnection(connectionString);
+        using (connection)
+        {
+          connection.Open();
+          var command = new SqlCommand(SqlScriptConstants.GET_ALL_PUPPIES_SQL, connection);
+
+          SqlDataReader reader = command.ExecuteReader();
+
+          if (reader.HasRows)
+          {
+            while (reader.Read())
+            {
+              var _pup = new Puppy()
+              {
+                Id = reader.GetInt32(0),
+                Name = reader.GetString(1),
+                Weight = reader.GetInt32(2),
+                Gender = reader.GetString(3),
+                PaperTrained = reader.GetBoolean(4)
+              };
+              rv.Add(_pup);
+            }
+          }
+          reader.Close();
+        }
+        return rv;
+      }
+      catch (SqlException ex)
+      {
+        throw;
+      }
+      catch (Exception ex)
+      {
+        throw;
+      }
     }
 
     /// <summary>
     /// Returns a specific puppy
     /// </summary>
-    /// <returns></returns>
+    /// <returns>Puppy</returns>
     public Puppy GetPuppy(int id)
     {
-      throw new NotImplementedException();
+      try
+      {
+        Puppy rv = null;
+        var connection = new SqlConnection(connectionString);
+        using (connection)
+        {
+          connection.Open();
+          var command = new SqlCommand(SqlScriptConstants.GET_PUPPY_SQL, connection);
+          command.Parameters.Add(new SqlParameter("id", id));
+
+          SqlDataReader reader = command.ExecuteReader();
+
+          if (reader.HasRows)
+          {
+            while (reader.Read())
+            {
+              var _pup = new Puppy()
+              {
+                Id = reader.GetInt32(0),
+                Name = reader.GetString(1),
+                Weight = reader.GetInt32(2),
+                Gender = reader.GetString(3),
+                PaperTrained = reader.GetBoolean(4)
+              };
+              rv = _pup;
+            }
+          }
+          reader.Close();
+        }
+        return rv;
+      }
+      catch (SqlException ex)
+      {
+        throw;
+      }
+      catch (Exception ex)
+      {
+        throw;
+      }
     }
 
     /// <summary>
     /// Saves a new puppy to the system.
     /// </summary>
     /// <param name="newPuppy"></param>
-    /// <returns></returns>
     public void SavePuppy(Puppy newPuppy)
     {
-      throw new NotImplementedException();
+      try
+      {
+        var connection = new SqlConnection(connectionString);
+        using (connection)
+        {
+          connection.Open();
+          var command = new SqlCommand(SqlScriptConstants.SAVE_PUPPY_SQL, connection);
+          command.Parameters.Add(new SqlParameter("name", newPuppy.Name));
+          command.Parameters.Add(new SqlParameter("weight", newPuppy.Weight));
+          command.Parameters.Add(new SqlParameter("gender", newPuppy.Gender));
+          command.Parameters.Add(new SqlParameter("paper_trained", newPuppy.PaperTrained));
+
+          _ = command.ExecuteNonQuery();
+        }
+      }
+      catch (SqlException ex)
+      {
+        throw;
+      }
+      catch (Exception ex)
+      {
+        throw;
+      }
     }
   }
 }
---
+++ src/MvcPuppies/Puppies.Web/DAL/SqlScriptConstants.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Puppies.Web.DAL
+{
+  public static class SqlScriptConstants
+  {
+    /// <summary>SELECTs all rows from the puppies table ordered by Id Asc (for consistency sake only)</summary>
+    public const string GET_ALL_PUPPIES_SQL =
+@"
+SELECT [id]
+      ,[name]
+      ,[weight]
+      ,[gender]
+      ,[paper_trained]
+FROM [dbo].[puppies]
+ORDER BY [id]
+";
+
+    /// <summary>SELECTs all rows from the puppies table</summary>
+    public const string GET_PUPPY_SQL =
+@"
+SELECT [id]
+      ,[name]
+      ,[weight]
+      ,[gender]
+      ,[paper_trained]
+FROM [dbo].[puppies]
+WHERE [id] = @id
+";
+
+    /// <summary>INSERTs a row into the puppies table</summary>
+    public const string SAVE_PUPPY_SQL =
+@"
+INSERT INTO [dbo].[puppies]
+           ([name]
+           ,[weight]
+           ,[gender]
+           ,[paper_trained])
+     VALUES
+           (@name
+           ,@weight
+           ,@gender
+           ,@paper_trained)
+";
+  }
+}
